2b2f3529120422bd5ebdbdc8e6a7fd2d7a9d6a23
naoki-haba <naosprintrunner6385@gmail.com>
2022-12-19 00:50:12 +0900

    feat:リクエストの扱い

--- src/main/main.go
+++ src/main/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/labstack/echo"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -11,16 +14,16 @@ func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello world")
 }
 
-type Cat struct {
+type Animal struct {
 	Name string `json:"name" query:"name"`
 	Type string `json:"type" query:"type"`
 }
 
 func getCats(c echo.Context) error {
-	var cat Cat
+	var a Animal
 	dataType := c.Param("data")
 
-	if err := c.Bind(&cat); err != nil {
+	if err := c.Bind(&a); err != nil {
 		return c.String(
 			http.StatusBadRequest,
 			fmt.Sprintf("Error is %s", err),
@@ -29,13 +32,13 @@ func getCats(c echo.Context) error {
 
 	if dataType == "json" {
 		return c.JSON(http.StatusOK, map[string]string{
-			"name": cat.Name,
-			"type": cat.Type,
+			"name": a.Name,
+			"type": a.Type,
 		})
 	}
 
 	if dataType == "string" {
-		return c.String(http.StatusOK, fmt.Sprintf("name is %s\n type is %s\n", cat.Name, cat.Type))
+		return c.String(http.StatusOK, fmt.Sprintf("name is %s\n type is %s\n", a.Name, a.Type))
 	}
 
 	return c.JSON(http.StatusBadRequest, map[string]string{
@@ -43,11 +46,71 @@ func getCats(c echo.Context) error {
 	})
 }
 
+func addCat(c echo.Context) error {
+	a := Animal{}
+
+	// レスポンスに関連するリソースを開放
+	defer c.Request().Body.Close()
+
+	// HTTPリクエストのボディを全て読み込む
+	// 注意: ioutil.ReadAll関数を使用する前に、必ずdefer c.Request().Body.Close()を呼び出すようにしてください。
+	// これは、リクエストのボディを閉じることで、リソースのリークを防ぐために必要です。
+	b, err := ioutil.ReadAll(c.Request().Body)
+	if err != nil {
+		log.Printf("ReadAll Error：%s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	// 指定されたJSONデータを構造体や別のデータ型に変換
+	err = json.Unmarshal(b, &a)
+	if err != nil {
+		log.Printf("Unmarshal Error：%s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	log.Printf("animal is %#v", a)
+	return c.String(http.StatusOK, "Success")
+}
+
+func addDog(c echo.Context) error {
+	a := Animal{}
+
+	// レスポンスに関連するリソースを開放
+	defer c.Request().Body.Close()
+
+	// json.NewDecoder関数を使用してデコーダを作成
+	// そのデコーダを使用してJSONデータを読み込み
+	// そのデータをaという構造体に変換
+	if err := json.NewDecoder(c.Request().Body).Decode(&a); err != nil {
+		log.Printf("Decode Error：%s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Printf("animal is %#v", a)
+	return c.String(http.StatusOK, "Success")
+}
+
+func addHamster(c echo.Context) error {
+	a := Animal{}
+
+	// HTTPリクエストのボディを構造体や別のデータ型にマッピングする
+	if err := c.Bind(&a); err != nil {
+		log.Printf("Bind Error：%s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Printf("animal is %#v", a)
+	return c.String(http.StatusOK, "Success")
+}
+
 func main() {
 	e := echo.New()
 
 	e.GET("/", hello)
 	e.GET("/cats/:data", getCats)
+	e.POST("/cats", addCat)
+	e.POST("/dog", addDog)
+	e.POST("/hamster", addHamster)
 
 	// サーバー起動
 	e.Logger.Fatal(e.Start(":1323"))
